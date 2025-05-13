b0b1e9edfb70d9f9403d06fb13d6513ad27701a4
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2025-05-13 21:19:21 +0530

    Update App.js

--- database_frontend/src/App.js
+++ database_frontend/src/App.js
@@ -1,11 +1,13 @@
 // App.js
 import React from 'react';
 import UserList from './UserList';
+import AddQuestion from './AddQuestion';
 
 function App() {
   return (
     <div className="App">
       <UserList />
+      <AddQuestion />
     </div>
   );
 }
