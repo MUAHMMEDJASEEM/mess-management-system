0afd127530a9e493434ecc97f616574c9c2af184
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2025-05-15 21:48:06 +0530

    looks changed

--- database_frontend/src/App.js
+++ database_frontend/src/App.js
@@ -1,16 +1,28 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import UserList from './UserList';
 import AddQuestion from './AddQuestion';
 import AddChapter from './AddChapter';
 
 function App() {
   return (
-    <div className="App">
-      <UserList />
-      <AddQuestion />
-      <AddChapter />
-    </div>
+    <Router>
+      <div className="App">
+        <nav style={{ marginBottom: '20px' }}>
+          <Link to="/" style={{ marginRight: '15px' }}>User List</Link>
+          <Link to="/add-question" style={{ marginRight: '15px' }}>Add Question</Link>
+          <Link to="/add-chapter">Add Chapter</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/add-question" element={<AddQuestion />} />
+          <Route path="/add-chapter" element={<AddChapter />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
